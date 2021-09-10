29113993486701425ed8b021fd432a2da599ac05
Simon Hopstätter <simon.hopstaetter@shipbit.de>
2021-09-10 16:39:10 +0200

    prerender index page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,7 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
 <h1>Oh hai! You did it.</h1>
 <p>
 	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation and
